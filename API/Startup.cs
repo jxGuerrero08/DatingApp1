17ef1da11300add8b801c4253c308043300b74a2
bong <jxguerrero@ialgorithmsolutions.com>
2022-09-06 13:26:56 +0800

    End of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -58,9 +58,10 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
